05ea27281154872570c51b732739b56cfac1f5f1
Leslie-John Richardson <lesliejohnrichardson@gmail.com>
2022-06-25 17:06:12 +0200

    Further work on Data Modularization

---
+++ Assets/Resources/Scripts/LooCast/Data/Attribute/Stat/StatsData.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LooCast.Data.Attribute.Stat
+{
+    [CreateAssetMenu(fileName = "StatsData", menuName = "Data/Attribute/Stat/StatsData", order = 0)]
+    public sealed class StatsData : ScriptableObject
+    {
+        public AgilityStatData AgilityStatData;
+        public AlertnessStatData AlertnessStatData;
+        public AwarenessStatData AwarenessStatData;
+        public BodyStatData BodyStatData;
+        public BrawnStatData BrawnStatData;
+        public CautiousnessStatData CautiousnessStatData;
+        public ChanceStatData ChanceStatData;
+        public CharmStatData CharmStatData;
+        public EgoStatData EgoStatData;
+        public EnduranceStatData EnduranceStatData;
+        public FateStatData FateStatData;
+        public FortitudeStatData FortitudeStatData;
+        public FortuneStatData FortuneStatData;
+        public IntellectStatData IntellectStatData;
+        public KnowledgeStatData KnowledgeStatData;
+        public MightStatData MightStatData;
+        public MindStatData MindStatData;
+        public PersonalityStatData PersonalityStatData;
+        public PowerStatData PowerStatData;
+        public PresenceStatData PresenceStatData;
+        public PsycheStatData PsycheStatData;
+        public QuicknessStatData QuicknessStatData;
+        public RecoveryStatData RecoveryStatData;
+        public ReflexesStatData ReflexesStatData;
+        public ResilienceStatData ResilienceStatData;
+        public ResistanceStatData ResistanceStatData;
+        public ResolveStatData ResolveStatData;
+        public SanityStatData SanityStatData;
+        public SenseStatData SenseStatData;
+        public SocialStatData SocialStatData;
+        public SpiritStatData SpiritStatData;
+        public StaminaStatData StaminaStatData;
+        public VitalityStatData VitalityStatData;
+        public WitsStatData WitsStatData;
+    } 
+}
