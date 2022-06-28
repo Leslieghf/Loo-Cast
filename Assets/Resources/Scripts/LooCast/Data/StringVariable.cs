c19a7730d0aa608a19a3d180b216696fbdcd2e47
Leslie-John Richardson <lesliejohnrichardson@gmail.com>
2022-06-28 18:34:17 +0200

    Enhanced ValueVariable classes

--- Assets/Resources/Scripts/LooCast/Data/StringVariable.cs
+++ Assets/Resources/Scripts/LooCast/Data/StringVariable.cs
@@ -1,10 +1,25 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace LooCast.Data
 {
     [CreateAssetMenu(fileName = "StringVariable", menuName = "Data/StringVariable", order = 0)]
     public class StringVariable : ScriptableObject
     {
-        public string Value;
+        [SerializeField] private string value;
+        public string Value
+        {
+            get
+            {
+                return value;
+            }
+
+            set
+            {
+                this.value = value;
+                OnValueChanged.Invoke();
+            }
+        }
+        public UnityEvent OnValueChanged;
     } 
 }
